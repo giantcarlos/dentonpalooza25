310bf3c49c5e98e3cfa67e4a158c7f0a4949cd28
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-09-01 13:24:52 -0500

    add hamburger to nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,13 +4,47 @@ import { HashLink } from 'react-router-hash-link';
 import { motion } from 'framer-motion';
 
 const NavBar = () => {
+    const [isOpen, setOpen] = useState(false)
+    const [active, setActive] = useState('nav-links')
+    const navToggle = () => {
+        active === 'nav-links' ? setActive('nav-links-active') : setActive('nav-links');
+        console.log(active);
+      }
+    const navOff = () => {
+        setActive('nav-links')
+        setOpen(false)
+    }
+
+    const scrollWithOffset = (el) => {
+        const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
+        const yOffset = -80; 
+        window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' }); 
+    }
+
+    const scrollWithOffset2 = (el) => {
+        const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
+        const yOffset = -50; 
+        window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' }); 
+    }
+
+    const scrollWithOffset3 = (el) => {
+        const yCoordinate = el.getBoundingClientRect().top + window.pageYOffset;
+        const yOffset = -120; 
+        window.scrollTo({ top: yCoordinate + yOffset, behavior: 'smooth' }); 
+    }
+  
+    const item = {
+        hidden: { y: -200 },
+        show: { y: 0 },
+      }
+      
     return (
         <nav style={{ padding: '1rem', background: '#222', color: '#fff' }}>
-            <motion.div variants={item} initial="hidden" animate="show" transition={{duration: 1, delay:.75}} className="header">DENTONPALOOZA 4
+            <motion.div variants={item} initial="hidden" animate="show" transition={{duration: 1, delay:.75}} className="header">DENTONPALOOZA 5
                 <div className="hamburger" onClick={navToggle}>
                 <Hamburger toggled={isOpen} toggle={setOpen}/>
                 </div>
-                <div className="sub-header">October 12, 2024 / Rubber Gloves Rehearsal Studios</div>
+                <div className="sub-header">October 11, 2025 / Dan's Silverleaf</div>
                 <div className={active} onClick={navOff}>
                     <HashLink smooth to="#" className="links" scroll={el => scrollWithOffset(el)}>HOME ➤</HashLink>
                     <HashLink smooth to="#lineup" className="links" scroll={el => scrollWithOffset2(el)}>LINEUP ➤</HashLink>
