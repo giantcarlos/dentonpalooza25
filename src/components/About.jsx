823e0b5ed06f1dc8516ca6b8dca0b4cf6435cec0
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-09-06 01:52:26 -0500

    add beneficiary placeholder logos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,8 +17,24 @@ function About() {
         delayChildren: 0,
         staggerChildren: 0.15
       }
+    }}
+
+    const container2 = {
+      hidden: { opacity: 0 },
+      show: {
+        opacity: 1,
+        transition: {
+          delayChildren: .5,
+          staggerChildren: 0.125
+        }
+      }
     }
-  }
+
+    const item2 = {
+      hidden: { opacity: 0 },
+      show: { opacity: 1 }
+    }
+  
 
   return (
     <section id="about">
@@ -27,6 +43,13 @@ function About() {
                 <div className="section-textbox">
                 <motion.h2 variants={item} className="section-header"  >WHAT IS DENTOPALOOZA?</motion.h2>
                 <motion.p variants={item} className="paragraph"  >DENTONPALOOZA 5 is a charity variety show showcasing the weird and beatiful people and performers in Denton, TX.</motion.p>
+                <motion.h2 variants={item} className="section-header2"  >BENEFICIARIES</motion.h2>
+                <motion.p variants={item} className="paragraph"  >All funds raised benefit these organizations:</motion.p>
+                <motion.div variants={container2} initial="hidden" whileInView="show" viewport={{ amount: .45, once: true }} className="beneficiary-grid">
+                            <motion.a variants={item2} href='https://www.missangelinesdenton.com/' className='image-reverse' target="_blank"><img src={"https://i.imgur.com/GBB5S1l.png"} alt='logo of Miss Angelines' /></motion.a>
+                            <motion.a variants={item2} href='https://danssilverleaf.com/' className='image-reverse-gray-zoom' target="_blank"><img src={"https://i.imgur.com/lU9nHkR.png"} alt='logo of Dans Silverleaf' /></motion.a>
+                            <motion.a variants={item2} href='https://www.eastsidedtx.com/' className='image-reverse-gray' target="_blank"><img src={"https://i.imgur.com/CWrmgQg.jpg"} alt='logo of Eastside' /></motion.a>
+                        </motion.div>
                 </div>
             </motion.div>
         </section>
