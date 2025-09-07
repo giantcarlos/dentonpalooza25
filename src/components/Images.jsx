c9e33798dacdd70075bc4ac2eace9bd9d1d93cd1
Gian Taino Carlos <giantainocarlos@gmail.com>
2025-09-06 22:35:44 -0500

    add subtle animation to image carousel

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -3,14 +3,21 @@ import { motion } from 'framer-motion';
 
 export default class Images extends Component {
   render() {
-    
+    const item = {
+      hidden: { y: 50,
+        opacity: 0
+       },
+      show: { y: 0,
+        opacity: 1
+       }
+    }
     return (
-      <div className='image-container'>
+      <motion.div variants={item} initial="hidden" animate="show" transition={{duration: .625}} className='image-container'>
         <div className='pic' id='pic4' alt="Von Erich brothers in their Iron Claw pose." />
         <div className='pic' id='pic3' alt="Carl Finch playing the accordion."/>
         <div className='pic' id='pic2' alt="Everything is Terrible on stage."/>
         <div className='pic' id='pic1' alt="Singer of Riverboat Gamblers crowdsurfing." />
-    </div>
+    </motion.div>
     )
   }
 }
